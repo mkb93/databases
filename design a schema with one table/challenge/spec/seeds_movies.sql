e24d74ce61fc229d595471306c9510355b34a6f2
marco belahouane <nfk18@hotmail.co.uk>
2023-04-25 17:48:11 +0100

    finished challenge 2.4

--- design a schema with one table/challenge/spec/seeds_movies.sql
+++ design a schema with one table/challenge/spec/seeds_movies.sql
@@ -1,6 +1,12 @@
+DROP TABLE IF EXISTS movies;
 CREATE TABLE movies (
   id SERIAL PRIMARY KEY,
   name text,
   genre text,
   release_year int
 );
+
+TRUNCATE TABLE movies RESTART IDENTITY; 
+
+INSERT INTO movies (name, genre, release_year) VALUES ('Pixies', 'Rock', 1), ('ABBA', 'Pop', 2);
+
