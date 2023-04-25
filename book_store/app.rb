3d272feca1ffce6e192b2f8035a86304c9eb34bc
marco belahouane <nfk18@hotmail.co.uk>
2023-04-25 16:57:40 +0100

    book_store finished

--- book_store/app.rb
+++ book_store/app.rb
@@ -1,13 +1,11 @@
 
 require_relative 'lib/database_connection'
-
+require_relative 'lib/book_store_repository'
 DatabaseConnection.connect('book_store')
 
 # Perform a SQL query on the database and get the result set.
-sql = 'SELECT id, title, author_name FROM books;'
-result = DatabaseConnection.exec_params(sql, [])
 
-# Print out each record from the result set .
-result.each do |book|
-  p book
-end
+books_repo = BookStoreRepository.new
+books_repo.all.each do |book|
+  puts  "#{book.id} - #{book.title} - #{book.author_name}"
+  end
--- book_store/lib/book_store_repository.rb
+++ book_store/lib/book_store_repository.rb
@@ -1,2 +1,18 @@
+require_relative "./book_store"
 class BookStoreRepository
+    def all
+      sql = 'SELECT id, title, author_name FROM books;'
+      result = DatabaseConnection.exec_params(sql, [])
+      book_store = []
+# Print out each record from the result set .
+      result.each do |book1|
+        book = BookStore.new
+        book.id = book1['id']
+        book.title = book1['title']
+        book.author_name = book1['author_name']
+
+        book_store << book
+      end
+       return book_store
+    end
 end
